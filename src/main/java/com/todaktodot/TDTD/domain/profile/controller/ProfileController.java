9a001fca40226d6cad028579fad2b9bd4cd00859
kyu-hyun <dlrbgus722@naver.com>
2026-02-14 03:44:46 +0900

    스웨거 수정

--- src/main/java/com/todaktodot/TDTD/domain/profile/controller/ProfileController.java
+++ src/main/java/com/todaktodot/TDTD/domain/profile/controller/ProfileController.java
@@ -1,11 +1,14 @@
 package com.todaktodot.TDTD.domain.profile.controller;
 
+import com.todaktodot.TDTD.domain.login.dto.request.LoginRequestDTO;
 import com.todaktodot.TDTD.domain.login.respository.entity.UserPrincipal;
 import com.todaktodot.TDTD.domain.profile.dto.request.SetNicknameRequestDTO;
 import com.todaktodot.TDTD.domain.profile.dto.response.SetNicknameResponseDTO;
 import com.todaktodot.TDTD.domain.profile.dto.response.UserDetailResponseDTO;
 import com.todaktodot.TDTD.domain.profile.service.ProfileService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -34,7 +37,8 @@ public class ProfileController {
     }
 
     @Operation(summary = "회원정보 조회", description = "로그인한 회원의 정보를 조회합니다.")
-    @ApiResponse(responseCode = "200", description = "회원 정보 조회 성공")
+    @ApiResponse(responseCode = "200", description = "회원 정보 조회 성공",
+            content = @Content(schema = @Schema(implementation = UserDetailResponseDTO.class)))
     @GetMapping("/detail")
     public ResponseEntity<UserDetailResponseDTO> getDetail(@AuthenticationPrincipal UserPrincipal userPrincipal) {
         long userId = userPrincipal.getId();
