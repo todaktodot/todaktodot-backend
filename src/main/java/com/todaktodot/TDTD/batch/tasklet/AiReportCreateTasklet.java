70a4328435352184bc30886a9f10a171f6820f8f
kyu-hyun <dlrbgus722@naver.com>
2026-04-07 22:54:48 +0900

    AI리포트 생성 날짜 범위 수정

--- src/main/java/com/todaktodot/TDTD/batch/tasklet/AiReportCreateTasklet.java
+++ src/main/java/com/todaktodot/TDTD/batch/tasklet/AiReportCreateTasklet.java
@@ -81,7 +81,7 @@ public class AiReportCreateTasklet implements Tasklet {
                     discordNotificationService.sendErrorNotificationForBatch(message);
                 }
             } catch (Exception e) {
-                String message = String.format("알수 없는 예외로 배치에서 AI 리포트 생성 실패, 커플 ID : %s, 메세지 : %s", couple.getCoupleId(), e.getMessage());
+                String message = String.format("알 수 없는 예외로 배치에서 AI 리포트 생성 실패, 커플 ID : %s, 메세지 : %s", couple.getCoupleId(), e.getMessage());
                 log.error(message);
                 discordNotificationService.sendErrorNotificationForBatch(message);
 
