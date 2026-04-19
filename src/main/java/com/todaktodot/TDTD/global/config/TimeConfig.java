75d09033ea75d797319cf46598872a5addc872c7
HJY <heoj10272@gmail.com>
2026-04-19 23:30:49 +0900

    feature/TDTDBE-97/타임존 설정

--- src/main/java/com/todaktodot/TDTD/global/config/TimeConfig.java
+++ src/main/java/com/todaktodot/TDTD/global/config/TimeConfig.java
@@ -1,15 +1,22 @@
 package com.todaktodot.TDTD.global.config;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Clock;
 import java.time.ZoneId;
+import java.util.TimeZone;
 
 @Configuration
 public class TimeConfig {
 
     private static final ZoneId ASIA_SEOUL = ZoneId.of("Asia/Seoul");
+
+    @PostConstruct
+    public void setDefaultTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone(ASIA_SEOUL));
+    }
 
     @Bean
     public Clock clock() {
